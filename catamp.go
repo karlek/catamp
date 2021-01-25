d83e63387403ad5fec673211d312a079dbaddd1c
Henry Eklind <henry@karlek.io>
2021-01-25 01:46:26 +0100

    Seek PoC

--- catamp.go
+++ catamp.go
@@ -1,59 +1,42 @@
 // It really whips the cat's ass.
 //
-//
+// Usage:
 // $ catamp *.flac
-//
-// Remote pausing is done with SIGTSTP
-// $ ps -C catamp | tail -n 1 | awk '{print $1}' | xargs -I '{}' kill -s SIGTSTP '{}'
-//
-// Remote playing is done with SIGCONT
-// $ ps -C catamp | tail -n 1 | awk '{print $1}' | xargs -I '{}' kill -s SIGCONT '{}'
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"flag"
 	"fmt"
-	"image"
-	"image/color"
-	"image/draw"
 	"io"
 	"log"
-	"math"
-	"math/rand"
-	"os"
-	"os/exec"
-	"runtime"
-	"strings"
-	"sync"
 	"time"
+	"strings"
+	"os"
+	"sync"
+	"os/exec"
 
-	"github.com/Sirupsen/logrus"
-	"github.com/karlek/profile"
-	"github.com/llgcode/draw2d/draw2dbase"
 	"github.com/mesilliac/pulse-simple"
 	"github.com/mewkiz/flac"
-	"github.com/mewmew/sdl/win"
-	"github.com/mewmew/we"
+	"github.com/sirupsen/logrus"
 )
 
-var (
-	width, height = 1280, 720
+const (
+	SeekSize = 44100*1
 )
 
 func main() {
-	defer profile.Start(profile.CPUProfile).Stop()
-	runtime.GOMAXPROCS(runtime.NumCPU())
 	flag.Parse()
 	if flag.NArg() < 1 {
 		flag.Usage()
 	}
 
-	// disable input buffering
+	// Disable input buffering.
 	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-	// do not display entered characters on the screen
+	// Do not display entered characters on the screen.
 	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-	// restore the echoing state when exiting
+	// Restore the echoing state when exiting.
 	defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
 
 	for _, f := range flag.Args() {
@@ -63,28 +46,36 @@ func main() {
 	}
 }
 
+func sampleFormat(stream *flac.Stream) (sf pulse.SampleFormat, size int) {
+	if stream.Info.BitsPerSample == 16 {
+		sf = pulse.SAMPLE_S16BE
+	} else if stream.Info.BitsPerSample == 24 {
+		sf = pulse.SAMPLE_S24_32BE
+	} else {
+		panic(fmt.Sprintf("Not implemented: bits per sample %d", stream.Info.BitsPerSample))
+	}
+	return sf, int(sf.SampleSize())
+}
+
 func catamp(filename string) (err error) {
-	stream, err := flac.ParseFile(filename)
+	f, err := os.Open(filename)
 	if err != nil {
-		fmt.Println(filename)
+		return err
+	}
+	defer f.Close()
+
+	stream, err := flac.NewSeek(f)
+	if err != nil {
 		return err
 	}
 	defer stream.Close()
 
-	// Number of bytes for a sample. Used by semi.
-	var size int
-
-	var sf pulse.SampleFormat
-	if stream.Info.BitsPerSample == 16 {
-		sf = pulse.SAMPLE_S24_32LE
-		size = 4
-	} else if stream.Info.BitsPerSample == 24 {
-		logrus.Warnln("not implemented - only one channel used")
-		sf = pulse.SAMPLE_S24_32BE
-		size = 8
+	sf, size := sampleFormat(stream)
+	ss := pulse.SampleSpec{
+		Format:   sf,
+		Rate:     stream.Info.SampleRate,
+		Channels: stream.Info.NChannels,
 	}
-	fmt.Println(stream.Info.BitsPerSample)
-	ss := pulse.SampleSpec{Format: sf, Rate: stream.Info.SampleRate, Channels: stream.Info.NChannels}
 	pb, err := initPulse(&ss)
 	if err != nil {
 		return err
@@ -93,49 +84,96 @@ func catamp(filename string) (err error) {
 	defer pb.Drain()
 
 	quit := make(chan bool)
-	go func() {
-		var b = []byte{0x00}
-		go func(quit chan bool) {
-			fmt.Print("                                                                      ")
-			for {
-				cat := []string{
-					"\r(^-.-^)ﾉ - Now playing: %s",
-					"\r(^._.^)ﾉ - Now playing: %s",
-					"\r(^+_+^)ﾉ - Now playing: %s",
-				}
-				for i := 0; i < len(filename); i++ {
-					select {
-					case <-quit:
-						return
-					default:
-						fmt.Printf(cat[i%len(cat)], strings.Repeat(filename+" ", 2)[i:i+len(filename)])
-						time.Sleep(200 * time.Millisecond)
-					}
-				}
-			}
-		}(quit)
-		for {
+	go catMarquee(filename, quit)
+	go userInput(stream, quit)
+
+	// Number of bytes for a sample.
+	go play(pb, stream, size, quit)
+	select {
+	case <-quit:
+		return
+	}
+	return nil
+}
+
+func userInput(stream *flac.Stream, quit chan bool) {
+	var b = make([]byte, 4)
+	rightArrowKey := []byte{27, 91, 67, 0}
+	leftArrowKey := []byte{27, 91, 68, 0}
+	for {
+		select {
+		case <-quit:
+			return
+		default:
+		}
+
+		os.Stdin.Read(b)
+		if b[0] == 'n' && stream != nil {
+			close(quit)
+			return
+		}
+		if b[0] == 'q' && stream != nil {
+			os.Exit(0)
+		}
+
+		var err error
+		if bytes.Equal(b, rightArrowKey) {
+			err = seekForward(stream)
+		}
+		if bytes.Equal(b, leftArrowKey) {
+			err = seekBack(stream)
+		}
+		if err != nil {
+			logrus.Errorln(err)
+		}
+	}
+}
+
+func catMarquee(filename string, quit chan bool) {
+	fmt.Print("                                                                      ")
+	for {
+		cat := []string{
+			"\r(^-.-^)ﾉ - Now playing: %s",
+			"\r(^._.^)ﾉ - Now playing: %s",
+			"\r(^+_+^)ﾉ - Now playing: %s",
+		}
+		for i := 0; i < len(filename); i++ {
 			select {
 			case <-quit:
 				return
 			default:
-				os.Stdin.Read(b)
-				if b[0] == 'n' && stream != nil {
-					close(quit)
-					stream.Seek(0, io.SeekEnd)
-					return
-				}
+				fmt.Printf(cat[i%len(cat)], strings.Repeat(filename+" ", 2)[i:i+20])
+				time.Sleep(200 * time.Millisecond)
 			}
 		}
-	}()
-	drawMusic(pb, stream, size)
-	// play(pb, stream, size)
-	select {
-	case <-quit:
-	default:
-		close(quit)
 	}
-	return nil
+}
+
+var globalSampleNum uint64
+
+var globalLock sync.Mutex
+func seekForward(stream *flac.Stream) error {
+	logrus.Println("Seeking forward")
+	globalLock.Lock()
+	fmt.Println("trying to seek to", globalSampleNum+SeekSize)
+	n, err := stream.Seek(globalSampleNum+SeekSize)
+	fmt.Println("seeked to", n)
+	fmt.Println("seeked to", n)
+	globalLock.Unlock()
+	logrus.Println(n/44100)
+	return err
+}
+
+func seekBack(stream *flac.Stream) error {
+	logrus.Println("Seeking backward")
+	globalLock.Lock()
+	fmt.Println("trying to seek to", globalSampleNum-SeekSize)
+	n, err := stream.Seek(globalSampleNum-SeekSize)
+	fmt.Println("seeked to", n)
+	fmt.Println("seeked to", n)
+	globalLock.Unlock()
+	logrus.Println(n/44100)
+	return err
 }
 
 func initPulse(ss *pulse.SampleSpec) (*pulse.Stream, error) {
@@ -147,228 +185,56 @@ func initPulse(ss *pulse.SampleSpec) (*pulse.Stream, error) {
 //
 // However, 24 bits per sample flac files are unable to play both channels at
 // the same time. Since I haven't been able to find a suitable SampleFormat.
-func play(pb *pulse.Stream, stream *flac.Stream, size int) {
-	var samples = []byte{}
+func play(pb *pulse.Stream, stream *flac.Stream, size int, quit chan bool) {
 	for {
-		n, err := readSamplesByte(stream, &samples, size)
+		select {
+		case <- quit:
+			return
+		default:
+		}
+		globalLock.Lock()
+		samples, n, err := readSamplesByte(stream, size)
+		globalSampleNum = n
+		globalLock.Unlock()
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
 			log.Fatalln(err)
 		}
-		pb.Write(samples[:n])
-		samples = samples[n:]
+		pb.Write(samples)
 	}
+	close(quit)
 }
 
-func readSamplesByte(stream *flac.Stream, samples *[]byte, size int) (n int, err error) {
-	tmp := make([]byte, size)
-	for {
-		frame, err := stream.ParseNext()
-		if err != nil {
-			return n, err
-		}
-		if stream.Info.BitsPerSample == 24 {
-			for i := 0; i < int(frame.BlockSize); i++ {
-				binary.BigEndian.PutUint32(tmp, uint32(frame.Subframes[0].Samples[i]))
-				*samples = append(*samples, tmp...)
-			}
-		} else {
-			for i := 0; i < int(frame.BlockSize); i++ {
-				for _, sf := range frame.Subframes {
-					binary.BigEndian.PutUint32(tmp, uint32(sf.Samples[i]))
-					*samples = append(*samples, tmp...)
-				}
-			}
-		}
-		if len(*samples) >= int(stream.Info.SampleRate)*size {
-			break
-		}
-	}
-	n = int(stream.Info.SampleRate) * size
-	if len(*samples) < n {
-		n = len(*samples)
-	}
-	return n, err
-}
-
-func readSamplesInt(stream *flac.Stream, samples *[]int32, read int) (n int, err error) {
-	for {
-		frame, err := stream.ParseNext()
-		if err != nil {
-			return n, err
-		}
-		if stream.Info.BitsPerSample == 24 {
-			for i := 0; i < int(frame.BlockSize); i++ {
-				*samples = append(*samples, frame.Subframes[0].Samples[i])
-			}
-		} else {
-			for i := 0; i < int(frame.BlockSize); i++ {
-				for _, sf := range frame.Subframes {
-					*samples = append(*samples, int32(sf.Samples[i]))
-				}
-			}
-		}
-		if len(*samples) >= read {
-			break
-		}
-	}
-	n = read
-	if len(*samples) < n {
-		n = len(*samples)
-	}
-	return n, err
-}
-
-func drawMusic(pb *pulse.Stream, stream *flac.Stream, size int) {
-	var samples = []int32{}
-
-	var max int
-	if size == 4 {
-		max = math.MaxInt16
-	} else if size == 8 {
-		max = math.MaxInt32 / 128
-	}
-	tmp := make([]byte, size)
-	read := int(stream.Info.BlockSizeMax)
-	// read := int(stream.Info.SampleRate / 8)
-	var n int
-	var err error
-	log.Println(read, stream.Info)
-	// if read < int(stream.Info.BlockSizeMin) {
-	// 	log.Fatalln("bad read size")
-	// }
-
-	// Open the window.
-	err = win.Open(width, height, win.Resizeable)
+func readSamplesByte(stream *flac.Stream, size int) ([]byte, uint64, error) {
+	frame, err := stream.ParseNext()
 	if err != nil {
-		log.Fatalln(err)
+		return nil, 0, err
 	}
-	defer win.Close()
+	samples := make([]byte, 0, int(frame.BlockSize))
 
-	normalized := make([]float64, int(stream.Info.SampleRate))
-	for {
-		var data = []byte{}
-		if len(samples) < read {
-			n, err = readSamplesInt(stream, &samples, read)
-			if err != nil {
-				if err == io.EOF {
-					break
-				}
-				log.Fatalln(err)
+	tmp := make([]byte, size)
+	if stream.Info.BitsPerSample == 24 {
+		for i := 0; i < int(frame.BlockSize); i++ {
+			for _, sf := range frame.Subframes {
+				binary.BigEndian.PutUint32(tmp, uint32(sf.Samples[i]))
+				samples = append(samples, tmp...)
 			}
-		} else {
-			n = read
 		}
-		for i, s := range samples[:n] {
-			normalized[i] = float64(s) / float64(max)
-			binary.BigEndian.PutUint32(tmp, uint32(s))
-			data = append(data, tmp...)
-		}
-		samples = samples[n:]
-
-		// Responsive window size.
-		lolfi, _ := win.Screen()
-		width, height = lolfi.Width, lolfi.Height
-		img := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{width, height}})
-		draw.Draw(img, img.Bounds(), image.Black, image.ZP, draw.Src)
-
-		wg := new(sync.WaitGroup)
-		wg.Add(1)
-		go func(wg *sync.WaitGroup) {
-			v0 := normalized[0]
-			for i := 2; i < n; i += 2 {
-				line(img, normalized[i], &v0, i, read, width, height)
-			}
-			wg.Done()
-		}(wg)
-		wg.Add(1)
-		go func(wg *sync.WaitGroup) {
-			v0 := normalized[1]
-			for i := 3; i < n; i += 2 {
-				line(img, normalized[i], &v0, i, read, width, height)
-			}
-			wg.Done()
-		}(wg)
-		wg.Wait()
-
-		// Change image.Image type to win.Image.
-		im, err := win.ReadImage(img)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		// Draw image to window.
-		err = win.Draw(image.ZP, im)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		// Display window updates on screen.
-		win.Update()
-		im.Free()
-
-		pb.Write(data)
-		data = data[n*size:]
-
-		// Poll events until the event queue is empty.
-		for e := win.PollEvent(); e != nil; e = win.PollEvent() {
-			switch e.(type) {
-			case we.Close:
-				os.Exit(0)
+	} else {
+		for i := 0; i < int(frame.BlockSize); i++ {
+			for _, sf := range frame.Subframes {
+				binary.BigEndian.PutUint16(tmp, uint16(sf.Samples[i]))
+				samples = append(samples, tmp...)
 			}
 		}
 	}
+	return samples, frame.SampleNumber(), err
 }
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage: %s [FILE],,,\n", os.Args[0])
 	flag.PrintDefaults()
 	os.Exit(1)
-}
-
-func r255() uint8 {
-	return uint8(rand.Intn(255))
-}
-
-func line(img draw.Image, v float64, v0 *float64, i, read, width, height int) {
-	// for i := 0; i < width; i++ {
-	// x := v*100 + math.Min(float64(width), float64(height))/5*math.Cos(v*float64(i)) + float64(width/2)
-	// y := math.Min(float64(width), float64(height))/5*math.Sin(v*float64(i)) + float64(height/2)
-	// x0 := (*v0)*100 + math.Min(float64(width), float64(height))/5*math.Cos((*v0)*float64(i-2)) + float64(width/2)
-	// y0 := (*v0)*100 + math.Min(float64(width), float64(height))/5*math.Sin((*v0)*float64(i-2)) + float64(height/2)
-	// img.Set(int(x), int(y), c)
-	// }
-	x, y := math.Floor(float64(i)/float64(read)*float64(width)), math.Floor(v*float64(height)/2+float64(height)/2)
-	x0, y0 := math.Floor(float64(i-2)/float64(read)*float64(width)), math.Floor((*v0)*float64(height)/2+float64(height)/2)
-	// c := color.RGBA{r255(), r255(), r255(), 255}
-	c := color.RGBA{uint8((float64(x) / float64(width)) * 255), 128, 190, 255}
-	draw2dbase.Bresenham(img, c, int(x0), int(y0), int(x), int(y))
-	img.Set(int(x), int(y), c)
-	draw2dbase.Bresenham(img, c, int(x0), int(y0), int(x), int(y))
-	(*v0) = v
-}
-
-func circle(img draw.Image, v float64, v0 *float64, i, read, width, height int) {
-
-	// for i := 0; i < width; i++ {
-	x := v*100 + math.Min(float64(width), float64(height))/5*math.Cos(v*float64(i)) + float64(width/2)
-	y := math.Min(float64(width), float64(height))/5*math.Sin(v*float64(i)) + float64(height/2)
-	c := color.RGBA{uint8((float64(x) / float64(width)) * 255), 128, 190, 255}
-	// x0 := (*v0)*100 + math.Min(float64(width), float64(height))/5*math.Cos((*v0)*float64(i-2)) + float64(width/2)
-	// y0 := (*v0)*100 + math.Min(float64(width), float64(height))/5*math.Sin((*v0)*float64(i-2)) + float64(height/2)
-	img.Set(int(x), int(y), c)
-	// }
-	// x = float64(width)*math.Cos(float64(i)/float64(read)) - float64(width/2)
-	// y := float64(height)*math.Sin(v*float64(height)) + float64(height/2)
-	// fmt.Println(x, y, v, v/float64(height))
-	// x0 := float64(width) * math.Acos((float64(i-2) / float64(read))) / math.Pi
-	// y0 := float64(height) * math.Asin(math.Floor((*v0)*float64(height)/2)) / math.Pi
-	// x, y := math.Floor(float64(i)/float64(read)*float64(width)), math.Floor(v*float64(height)/2+float64(height)/2)
-	// x0, y0 := math.Floor(float64(i-2)/float64(read)*float64(width)), math.Floor((*v0)*float64(height)/2+float64(height)/2)
-	// c := color.RGBA{r255(), r255(), r255(), 255}
-	// draw2dbase.Bresenham(img, c, int(x0), int(y0), int(x), int(y))
-	// img.Set(int(x), int(y), c)
-	// draw2dbase.Bresenham(img, c, int(x0), int(y0), int(x), int(y))
-	// (*v0) = v
 }
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/karlek/catamp
+
+go 1.15
+
+require (
+	github.com/karlek/profile v0.0.0-20150530082700-5ec864786f0f
+	github.com/llgcode/draw2d v0.0.0-20200930101115-bfaf5d914d1e
+	github.com/mesilliac/pulse-simple v0.0.0-20170506101341-75ac54e19fdf
+	github.com/mewkiz/flac v1.0.7-0.20210125003852-a8b6d7e0df08
+	github.com/mewkiz/pkg v0.0.0-20210112042322-0b163ae15d52 // indirect
+	github.com/mewmew/sdl v0.0.0-20140124163006-46fb5532bfce
+	github.com/mewmew/we v0.0.0-20181024063015-78d4f17407b5
+	github.com/sirupsen/logrus v1.7.0
+	golang.org/x/image v0.0.0-20201208152932-35266b937fa6 // indirect
+	golang.org/x/sys v0.0.0-20210123111255-9b0068b26619 // indirect
+)
